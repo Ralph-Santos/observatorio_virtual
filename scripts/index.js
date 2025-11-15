1a1ca32a59a4eed9371f14a13790a98da28d190f
Ralph Santos <rb20102001@gmail.com>
2025-11-14 21:20:38 -0300

    usando divFather

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,15 @@
 const planos = [
   {
     tipo: "Media Salarial",
-    id: "mapa1",
     titulo: "Mapa Media Salarial",
     ref: "mapas/mapa_capitais.html",
     detalhes: [
       "Visualização interativa",
       "Média salarial por capital"
-    ]
+    ],
+    image: './img/jg.jpeg'
   },
-   {
+  {
     tipo: "Media Salarial",
     id: "mapa1",
     titulo: "Mapa Media Salarial",
@@ -19,7 +19,7 @@ const planos = [
       "Média salarial por capital"
     ]
   },
-   {
+  {
     tipo: "Media Salarial",
     id: "mapa1",
     titulo: "Mapa Media Salarial",
@@ -29,7 +29,7 @@ const planos = [
       "Média salarial por capital"
     ]
   },
-   {
+  {
     tipo: "Media Salarial",
     id: "mapa1",
     titulo: "Mapa Media Salarial",
@@ -39,7 +39,7 @@ const planos = [
       "Média salarial por capital"
     ]
   },
-   {
+  {
     tipo: "Media Salarial",
     id: "mapa1",
     titulo: "Mapa Media Salarial",
@@ -53,18 +53,29 @@ const planos = [
 
 const planosContainer = document.getElementById("planos");
 
+
 function renderPlanos() {
   planosContainer.innerHTML = "";
 
   planos.forEach(plano => {
+    const img = document.createElement("img");
     const div = document.createElement("div");
+    const divFather = document.createElement("div");
     div.classList.add("plano");
-    div.innerHTML = `
+    //div.style.background = `url('${plano.image || './img/jg.jpeg'}') center top/cover`;
+    divFather.classList.add("plano-father");
+    divFather.innerHTML = `
+    ${div.innerHTML = `
       <h3>${plano.titulo}</h3>
       <ul>${plano.detalhes.map(item => `<li>${item}</li>`).join("")}</ul>
-      <button onclick="window.location.href='${plano.ref || "#"}'">Visualizar mapa</button>
-    `;
-    planosContainer.appendChild(div);
+      ${img.innerHTML = `
+        <img src='${plano.image || './img/jg.jpeg'}' alt='Imagem do mapa' style='width:100%; height:auto; border-radius:10px; margin-bottom:10px;' />
+        `}`}
+
+      <button onclick="window.location.href='${plano.ref || "#"}'">Visualizar mapa</button>`
+
+    planosContainer.appendChild(divFather);
+    divFather.appendChild(div);
   });
 }
 
