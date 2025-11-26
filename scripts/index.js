36c39d566c337725d9aec44a99eeea2f05a8b19e
Ralph Santos <rb20102001@gmail.com>
2025-11-26 16:30:06 -0300

    Adicionando mapas finais

--- scripts/index.js
+++ scripts/index.js
@@ -1,68 +1,42 @@
 const planos = [
   {
-    tipo: "Região Norte",
-    id: "mapa-norte",
-    titulo: "Informalidade no Norte",
+    tipo: "Mapa do trabalho informal no Brasil",
+    id: "mapa-desigualdade",
+    titulo: "Mapa do trabalho informal no Brasil",
     
-    ref: "mapas/mapa_norte.html", 
+    ref: "mapas/mapa_desigualdade_mulher_negra.html", 
     // Imagem: Cerâmica/Cultura Indígena ou Amazônia
     imagem: "https://s2.static.brasilescola.uol.com.br/be/2023/08/cultura-norte-ceramica-marajoara.jpg", 
     detalhes: [
-      "Taxas acima de 50%",
-      "Baixa contribuição previdenciária",
-      "Trabalho por 'bicos'"
+      "Trabalho informal no Brasil",
+      "Desigualdade social e econômica",
+      "Mulheres negras"
     ]
   },
   {
-    tipo: "Região Nordeste",
-    id: "mapa-nordeste",
-    titulo: "Informalidade no Nordeste",
+    tipo: "Desigualdade de gênero",
+    id: "mapa-genero",
+    titulo: "Desigualdade de gênero no Brasil",
     ref: "mapas/mapa_genero.html",
     // Imagem: Cultura/Pelourinho ou Sertão
     imagem: "https://brazilchannel.com.br/wp-content/uploads/2023/10/A-diversidade-cultural-dos-estados-do-Nordeste.jpg",
     detalhes: [
-      "Empreendedorismo de necessidade",
-      "Uberização e Desemprego",
-      "Impacto nas periferias"
+      "Desigualdade salarial",
+      "Média de pessoas em trabalho informal",
+      "Homens e Mulheres"
     ]
   },
   {
-    tipo: "Região Centro-Oeste",
-    id: "mapa-centro-oeste",
-    titulo: "Informalidade no Centro-Oeste",
-    ref: "mapas/mapa_centro-oeste.html",
+    tipo: "Desigualdade racial",
+    id: "mapa-racial",
+    titulo: "Desigualdade racial no Brasil",
+    ref: "mapas/mapa_raca.html",
     // Imagem: Pantanal ou Brasília
     imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Congresso_Nacional_-_Bras%C3%ADlia_-_Distrito_Federal_-_Brasil_02.jpg/800px-Congresso_Nacional_-_Bras%C3%ADlia_-_Distrito_Federal_-_Brasil_02.jpg",
     detalhes: [
-      "Setor de Construção Civil",
-      "Gig Economy",
-      "Falta de vínculos"
-    ]
-  },
-  {
-    tipo: "Região Sudeste",
-    id: "mapa-sudeste",
-    titulo: "Informalidade no Sudeste",
-    ref: "mapas/mapa_sudeste.html",
-    // Imagem: São Paulo/Rio Urbana
-    imagem: "https://images.unsplash.com/photo-1580619305218-8423a7ef79b4?q=80&w=1000&auto=format&fit=crop",
-    detalhes: [
-      "Motoristas de App e Entregadores",
-      "Consciência da precarização",
-      "Alta densidade urbana"
-    ]
-  },
-  {
-    tipo: "Região Sul",
-    id: "mapa-sul",
-    titulo: "Informalidade no Sul",
-    ref: "mapas/mapa_sul.html",
-    // Imagem: Araucárias ou Paisagem Sulista
-    imagem: "https://www.melhoresdestinos.com.br/wp-content/uploads/2020/08/gramado-capa2019-01.jpg",
-    detalhes: [
-      "Exclusão de direitos sociais",
-      "Desigualdades de gênero",
-      "Estratégias de sobrevivência"
+      "Desigualdade salarial",
+      "Média de pessoas em trabalho informal",
+      "Taxa de pessoas na informalidade por raça"
     ]
   }
 ];
